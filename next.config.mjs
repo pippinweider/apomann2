9c68773dcb4340d4c4688c3b883fa81edb62f503
pippinweider <pippinweider@gmail.com>
2024-09-21 22:51:01 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,24 @@
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let assetPrefix = '';
+let basePath = '';
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '');
+  assetPrefix = `/${repo}/`;
+  basePath = `/${repo}`;
+}
+
 const nextConfig = {
   images: {
-    unoptimized: true,
+    unoptimized: true,  // Keep your existing configuration
   },
+  assetPrefix: assetPrefix,  // For GitHub Pages
+  basePath: basePath,        // For GitHub Pages
+  trailingSlash: true,       // Ensure URLs end with a slash for static file serving
 };
 
 export default nextConfig;
+
